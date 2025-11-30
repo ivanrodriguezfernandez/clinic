a4b688303df0e5842dcea710a756dd5c02f27343
Iván Rodríguez <ivanrodriguezfernandez@gmail.com>
2025-11-30 15:20:04 +0100

    fix: convert jest.config.js to ES modules format
    - Change from module.exports to export default
    - Add extensionsToTreatAsEsm configuration
    - Add moduleNameMapper for .js extensions
    - Configure ts-jest to use ESM
    - Fixes Jest compatibility with ES modules

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,19 @@
-module.exports = {
+export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/src', '<rootDir>/tests'],
   testMatch: ['**/*.test.ts', '**/*.spec.ts'],
   moduleFileExtensions: ['ts', 'js', 'json'],
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
 };
